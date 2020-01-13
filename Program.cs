21b2970de161a74d8fa6cb83307ccdfcb96b5f00
tonyverdin <52440419+tonyverdin@users.noreply.github.com>
2020-01-13 11:43:07 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace ProgrammingChallege1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Palandrome a = new Palandrome("t");
+            Console.WriteLine(a.TestPalandrome());
+
+        }
+    }
+}
